3c8351b45d9d61145ed93b0effaf9f246369bc29
hatae <hata676@naver.com>
2020-11-10 07:24:42 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -2,13 +2,13 @@ const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
-  const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : `${hours}`}:${
-    minutes < 10 ? `0${minutes}` : `${minutes}`
-  }:${seconds < 10 ? `0${seconds}` : `${seconds}`}`;
+  const seconds = 720*3600;
+
+  seconds--;
+  
+  clockTitle.innerText = `${seconds/3600 < 10 ? `0${seconds/3600}` : `${seconds/3600}`}:${
+    seconds%3600/60 < 10 ? `0${seconds%3600/60}` : `${seconds%3600/60}`
+  }:${seconds%3600%60 < 10 ? `0${seconds%3600%60}` : `${seconds%3600%60}`}`;
 }
 
 function init() {
